1b8b1baac053cda3c0fdbcd3d0e0a7dea5a873df
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-07-11 17:15:29 +0200

    First commit

---
+++ src/main/java/com/kodilla/foodpairingfrontend/FoodPairingFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.foodpairingfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPairingFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodPairingFrontendApplication.class, args);
+    }
+
+}
